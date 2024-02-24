37320ebb9819dbc69722113e5f11eb5cdcb6ada4
Piyush Kumar <piyushkr446@gmail.com>
2024-02-24 15:41:39 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
 import Home from './components/Home'
-import Anime from './components/Anime';
 import Movies from './components/Movies';
 import Music from './components/Music';
 import Games from './components/Games';
@@ -14,7 +13,6 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/Anime' element={<Anime/>} />
         <Route path='/Movies' element={<Movies/>} />
         <Route path='/Music' element={<Music/>} />
         <Route path='/Games' element={<Games/>} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,6 @@ function Header() {
         <div className='text-xl bg-blue-500 p-2 text-white rounded-full'>FlixSonic</div>
         <div className='flex gap-5 items-center border-2 rounded-full bg-blue-500 text-white p-2'>
             <NavLink to='/' className='hover:text-black'>Home</NavLink>
-            <NavLink to='Anime' className='hover:text-black'>Anime</NavLink>
             <NavLink to='Movies' className='hover:text-black'>Movies</NavLink>
             <NavLink to='Music' className='hover:text-black'>Music</NavLink>
             <NavLink to='Games' className='hover:text-black'>Games</NavLink>
